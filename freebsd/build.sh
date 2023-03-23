66104fc12e304114a7eb1689ab6ec2b7fb9fa768
philippe44 <philippe44@users.noreply.github.com>
2023-03-22 18:17:12 -0700

    update for solaris & freebsd
    better use of gcc 11.x but need binutils 2.40 to get read of a coe relocation error (was using a compiler workaround -mno-direct-extern-access

--- freebsd/build.sh
+++ freebsd/build.sh
@@ -16,12 +16,12 @@ mkdir -p $PREFIX/$TARGET
 tar xvf freebsd.$1.tgz --directory $PREFIX/$TARGET || exit
 
 # https://ftp.gnu.org/gnu/binutils/binutils-2.39.tar.xz
-item=binutils-2.39
+item=binutils-2.40
 rm -rf $item
 tar xvf $item.tar.xz 
 pushd $item
 ./configure --enable-libssp --enable-ld --target=$TARGET --prefix=$PREFIX --with-sysroot=$SYSROOT
-make -j8 && make install
+make -j16 && make install
 popd
 
 # https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
@@ -30,7 +30,7 @@ rm -rf $item
 tar xvf $item.tar.xz 
 pushd $item
 ./configure --prefix=$PREFIX --enable-shared --enable-static --enable-mpbsd --enable-fft --enable-cxx --host=$TARGET
-make -j8 && make install
+make -j16 && make install
 popd
 
 # https://www.mpfr.org/mpfr-current/mpfr-4.1.0.tar.xz
@@ -39,7 +39,7 @@ rm -rf $item
 tar xvf $item.tar.xz 
 pushd $item
 ./configure --prefix=$PREFIX --with-gnu-ld  --enable-static --enable-shared --with-gmp=$PREFIX --host=$TARGET
-make -j8 && make install
+make -j16 && make install
 popd
 
 # https://ftp.gnu.org/gnu/mpc/mpc-1.2.1.tar.gz
@@ -48,7 +48,7 @@ rm -rf $item
 tar xvf $item.tar.gz 
 pushd $item
 ./configure --prefix=$PREFIX --with-gnu-ld --enable-static --enable-shared --with-gmp=$PREFIX --with-mpfr=$PREFIX --host=$TARGET
-make -j8 && make install
+make -j16 && make install
 popd
 
 # https://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz
@@ -57,20 +57,20 @@ rm -rf $item
 tar xvf $item.tar.gz 
 pushd $item
 ./configure --prefix=$PREFIX --enable-static --enable-shared --host=$TARGET --with-sysroot=$SYSROOT --program-prefix=$TARGET-
-make -j8 && make install
+make -j16 && make install
 popd
 
 # gcc
 # the build-sysroot must be set because the sysroot is a subpath of prefix. In that case, the 
 # configure script makes is relative to where gcc is, so that it's relocatable. Unfortunately, 
 # that works for USING the compiler, but not for BUILDING it (during this phase below)
-item=gcc-12.2.0
+item=gcc-11.2.0
 rm -rf $item
 tar xvf $item.tar.xz 
 pushd $item
 mkdir -p build && cd build
 ../configure --without-headers --with-gnu-as --with-gnu-ld --disable-nls --enable-languages=c,c++ --enable-libssp --enable-ld --disable-libitm --disable-libquadmath --target=$TARGET --prefix=$PREFIX --with-gmp=$PREFIX --with-mpc=$PREFIX --with-mpfr=$PREFIX --disable-libgomp --with-sysroot=$SYSROOT --with-build-sysroot=$SYSROOT
-make -j8 && make install
+make -j16 && make install
 popd
 
 exit
